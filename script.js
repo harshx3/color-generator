55d8d84041e5e1a57d6cfdbc23ff7bc9bc52f48c
Harsh-Makwana <harshmakwana686@gmail.com>
2024-07-20 23:36:32 -0400

    Copy Function Added

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const containerEl = document.querySelector(".container");
+let colorContainerEl;
+
+const copyToClipboard = (event) => {
+    console.log('clipboard call');
+    const clickedDiv = event.target;
+    const copyText = clickedDiv.textContent;
+    console.log(copyText);
+}
 
 for(let i=0; i<30; i++){
-    const colorContainerEl = document.createElement("div");
+    colorContainerEl = document.createElement("div");
     colorContainerEl.classList.add("color-container");
     containerEl.appendChild(colorContainerEl);
     const color = selectedColor();
     const textNode = document.createTextNode(`${color}`);
     colorContainerEl.appendChild(textNode);
-    colorContainerEl.style.backgroundColor = `${color}`
+    colorContainerEl.style.backgroundColor = `${color}`;
+    colorContainerEl.style.cursor = 'pointer';
 
+    colorContainerEl.addEventListener('click', copyToClipboard);
 }
+
+
+
+
+
+
 
 
 function selectedColor() {
     const color = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
     let setColor = ['#'];
-    let pickedColor;
     for(var i=1; i<=6; i++) {
        let pick = Math.floor(Math.random()* color.length);
       setColor.push(color[pick]);
-      console.log(setColor.toString().replaceAll(',',''));
     }
-   
+   return setColor.toString().replaceAll(',','');
     
 }
+
